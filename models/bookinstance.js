6dcba12b4e58fc7955c74bed4f5f28cb80bf336a
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-09-27 16:13:18 -0400

    Add due_back_yyyy_mm_dd()

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -20,5 +20,9 @@ BookInstanceSchema.virtual("url").get(function () {
   return `/catalog/bookinstance/${this._id}`;
 });
 
+BookInstanceSchema.virtual("due_back_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.due_back).toISODate(); // format 'YYYY-MM-DD'
+});
+
 // Export model
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
