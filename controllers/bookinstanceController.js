124881bf406d95b571744168c21f569effb0e13f
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-10-02 16:13:27 -0400

    Complete bookinstance_delete_get/post

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -93,12 +93,24 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+  const instance = await BookInstance.findById(req.params.id).exec();
+
+  if (instance === null) res.redirect("/catalog/bookinstances");
+
+  res.render("bookinstance_delete", {
+    title: "Delete Book Instance",
+    instance,
+  });
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+  const instance = BookInstance.findById(req.params.id).exec();
+
+  if (instance === null) res.redirect("/catalog/bookinstances");
+
+  await BookInstance.findByIdAndRemove(req.params.id);
+  res.redirect("/catalog/bookinstances");
 });
 
 // Display BookInstance update form on GET.
