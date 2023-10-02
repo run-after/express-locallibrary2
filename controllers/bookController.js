e0f99e5841082af912b0ca88b3f1da7ebd4ceb0f
Chase Stearn <53063763+run-after@users.noreply.github.com>
2023-10-02 16:22:17 -0400

    Complete book_delete_post

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -170,7 +170,22 @@ exports.book_delete_get = asyncHandler(async (req, res, next) => {
 
 // Handle book delete on POST.
 exports.book_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete POST");
+  const [book, allBookInstances] = await Promise.all([
+    Book.findById(req.params.id).exec(),
+    BookInstance.find({ book: req.params.id }),
+  ]);
+
+  if (allBookInstances.length > 0) {
+    res.render("book_delete", {
+      title: "Delete Book",
+      book,
+      instances: allBookInstances,
+    });
+    return;
+  } else {
+    await Book.findByIdAndRemove(req.body.id);
+    res.redirect("/catalog/books");
+  }
 });
 
 // Display book update form on GET.
